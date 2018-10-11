9b5207f9635f10e654aa6bbcc3c9daef0854b5e4
Dalton Carr <daltonlcarr@yahoo.com>
2018-10-10 18:13:28 -0700

    just adding the simple javascript only version for now to get moving on other projects

--- simple-tip/app.js
+++ simple-tip/app.js
@@ -1,5 +1,9 @@
 'use strict';
-// function multiplyBy()
+
+//OPTIONONE_______________________________________________________________
+
+
+// function newArray()
 // {
 //         const dollarhour = document.getElementById("firstNumber").value;
 //         const array = document.getElementById("secondNumber").value;
@@ -12,13 +16,60 @@
 // }
 
 
-function multiplyBy() {
-var moneyperhour = document.getElementById("firstNumber").value;
-var array = document.getElementById("secondNumber").value;
-    for(var i = 0; i < array.length; i++)
-    {
-       let hi = array * moneyperhour;
-         document.getElementById("result").innerHTML = hi;
-    }
+
+
+//OPTIONTWO_______________________________________________________________
+
+// function newArray() {
+//     var moneyperhour = document.getElementById("firstNumber").value;
+//     var array = document.getElementById("secondNumber").value;
+//         for(var i = 0; i < array.length; i++)
+//         {
+//            let hi = array * moneyperhour;
+//              document.getElementById("result").innerHTML = hi;
+//         }
+        
+//     }
     
-}
+
+
+
+
+//OPTIONTHREE_______________________________________________________________
+// const array = document.getElementById("secondNumber");
+// const dollarperhour = document.getElementById("firstNumber");
+
+
+// function newArray(array,dollarperhour){
+//     const result = array.map(function(x){
+//         return x * dollarperhour;
+//     })
+//     return result;
+
+//   }
+
+//   document.getElementById("result").innerHTML = newArray;
+
+
+
+// FINAL EASY SOLUTION TO JUST BE CALLED ON SCREW FRONT END____________________________
+//Here fill in how many hours each person has with commas inbetween.
+let HoursPerWeek = [1,2,3];
+
+//Here fill in how many dollars per hour we made. (How much cash divided by total hours)
+let DollarPerHour = 18;
+
+
+function newArray(array,dollarperhour){
+    const result = array.map(function(x){
+        return x * dollarperhour;
+    })
+    return result;
+
+  }
+
+
+  newArray(HoursPerWeek,DollarPerHour);
+
+
+
